15af206d2ee9b25cf9204fb5069eab62e9f367af
josi-asae <josi.asae@gmail.com>
2017-07-03 00:02:19 +0100

    python rotational cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,24 @@
-def rotate():
-    pass
+def rotate(message, shift):
+    newMessage = ''
+    # loop over characters in message
+    for letter in range(0,len(message)):
+        # convert character to number
+        letterKey = ord(message[letter])
+        # check if character is a lower case letter
+        if letterKey > 96 and letterKey <= 96 + 26:
+            newLetterKey = (letterKey - 96 + shift)%26
+            if newLetterKey == 0:
+                newLetterKey = 26
+            newLetter = chr(newLetterKey + 96)
+        # check if the character is an upper case letter
+        elif letterKey > 64 and letterKey <= 64 + 26:
+            newLetterKey = (letterKey - 64 + shift)%26
+            if newLetterKey == 0:
+                newLetterKey = 26
+            newLetter = chr(newLetterKey + 64)
+        # if character isn't a letter, don't change it
+        else:
+            newLetter = message[letter]
+        # compose new message
+        newMessage = newMessage + newLetter
+    return newMessage
